6e7315fdf4e45e53fa666a1f07d2349ce557f3b5
lukaskiss222 <lukaskiss222@gmail.com>
2018-10-15 21:45:37 +0200

    preperation for first run

--- revolve/start.py
+++ revolve/start.py
@@ -8,15 +8,14 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from revolve.util import Supervisor
 
 if __name__ == "__main__":
-
     supervisor = Supervisor(
-        manager_cmd=None,
-        world_file="worlds/gait-learning.world",
-        gazebo_cmd="gazebo",
-        gazebo_args=["--verbose"],
-        plugins_dir_path=os.path.join(rvpath, 'build', 'lib'),
-        models_dir_path=os.path.join(rvpath, 'models')
+            manager_cmd=os.path.join(here, "tutorial4.py"),
+            world_file="worlds/gait-learning.world",
+            gazebo_cmd="gazebo",
+            gazebo_args=["--verbose"],
+            plugins_dir_path=os.path.join(rvpath, 'build', 'lib'),
+            models_dir_path=os.path.join(rvpath, 'models')
     )
 
-    ret = supervisor.launch_gazebo()
+    ret = supervisor.launch()
     sys.exit(ret)
